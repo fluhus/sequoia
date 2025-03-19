14429d30b6a6c17bb5476039cd4575b8825dac26
Amit Lavon <amitlavon1.spam@gmail.com>
2025-03-19 08:31:29 -0700

    Extract abundance loading to its own package

---
+++ abundance.py
@@ -0,0 +1,107 @@
+import json
+from glob import glob
+
+import numpy as np
+import pandas as pd
+
+from samplenaming import fix_name
+
+SPIKE_TAXA = [
+    'NC_003045',  # Bovine covid
+    'NC_006213',  # Human coronavirus OC43
+    'NC_001405',  # Human mastadenovirus C
+    'NC_001454',  # Human mastadenovirus F
+    'NC_001401',  # Adeno-associated virus 2
+    # Tax IDs in Kraken
+    '694003',
+    '129951',
+    '130309',
+    '2955291',
+    '1986019',
+    '130310',
+    # COVID spike in bundy
+    'Bovine coronavirus',
+    'Human coronavirus OC43',
+    # COVID spike in kraken
+    'Betacoronavirus 1',
+    'Rabbit coronavirus HKU14',
+    # COVID spike in CZID
+    'Betacoronavirus',
+    # Adenovirus
+    'Human adenovirus 1',
+    'Human mastadenovirus A',
+    'Human mastadenovirus C',
+    'Human mastadenovirus F',
+    'Human mastadenovirus G',
+    'Simian mastadenovirus B',
+    'Simian mastadenovirus C',
+    'Simian mastadenovirus F',
+    'adeno-associated virus 2',
+    'Mastadenovirus',
+    # Flu
+    'Alphainfluenzavirus influenzae',
+]
+
+
+def load_data(
+    glb: str,
+    remove_spike: bool | str,
+    exp_files=36,
+    normalize=True,
+    phylum_mode=False,
+) -> pd.DataFrame:
+    files = glob(glb)
+    files = [f for f in files if 'Undetermined' not in f]
+    if exp_files:
+        assert len(files) == exp_files, f'expected {exp_files}, got {len(files)}'
+    print(len(files), 'files')
+    jsons = (json.load(open(f)) for f in files)
+
+    df = (
+        pd.DataFrame(jsons)
+        .assign(name=[fix_name(f) for f in files])
+        .set_index('name')
+        .replace(np.nan, 0)
+        .sort_index()
+    )
+    df = sort_columns(df)
+
+    if phylum_mode:
+        # Split columns into species and phylum.
+        # Using species for spike removal, then assigning phylum again.
+        spc, phy = [], []
+        for c in df.columns.tolist():
+            s = c.split(',')
+            assert len(s) == 2, f'{c!r}: {len(s)} parts'
+            spc.append(s[0])
+            phy.append(s[1])
+        df.columns = spc
+
+    if remove_spike:
+        keep = [True] * df.shape[1]
+        print('Removing spike:')
+        for x in SPIKE_TAXA:
+            if x in df:
+                if type(remove_spike) is str:
+                    df[remove_spike] += df[x]
+                keep[df.columns.get_loc(x)] = False
+                # del df[x]
+                print('-', x)
+        df = df.loc[:, keep]
+
+    if phylum_mode:
+        # Sum by phylum.
+        phy = [x for x, k in zip(phy, keep) if k]
+        df.columns = phy
+        df = df.groupby(df.columns, axis=1).sum()
+
+    if normalize:  # Normalize to 1
+        df = df.div(df.sum(axis=1), axis=0)
+
+    return df
+
+
+def sort_columns(df: pd.DataFrame) -> pd.DataFrame:
+    column_sums = df.sum(axis=0)
+    column_sums = column_sums.sort_values(ascending=False)
+    return df.reindex(columns=column_sums.index)
--- jason.py
+++ jason.py
@@ -7,7 +7,6 @@ import json
 import re
 import sys
 from collections import defaultdict, namedtuple
-from glob import glob
 from typing import Iterable
 
 import numpy as np
@@ -20,6 +19,7 @@ from skbio.stats.distance import DistanceMatrix, permanova
 from sklearn.decomposition import PCA
 from sklearn.manifold import MDS
 
+from abundance import load_data, sort_columns
 from confidence_ellipse import confidence_ellipse
 from samplenaming import LUNA_GROUPS, fix_name, fix_name2, sample_group
 
@@ -44,100 +44,6 @@ ABND_KRAKEN_TIDS = f'{DATA_DIR}/ww-kraken/*.tid.json'
 ABND_KRAKEN_GEN = f'{DATA_DIR}/ww-kraken/*.gen.json'
 ABND_KRAKEN_PHY = f'{DATA_DIR}/ww-kraken/*.phy.json'
 ABND_KRAKEN_VIROR = f'{DATA_DIR}/ww-kraken/*.viror.json'
-
-SPIKE_TAXA = [
-    'NC_003045',  # Bovine covid
-    'NC_006213',  # Human coronavirus OC43
-    'NC_001405',  # Human mastadenovirus C
-    'NC_001454',  # Human mastadenovirus F
-    'NC_001401',  # Adeno-associated virus 2
-    # Tax IDs in Kraken
-    '694003',
-    '129951',
-    '130309',
-    '2955291',
-    '1986019',
-    '130310',
-    # COVID spike in bundy
-    'Bovine coronavirus',
-    'Human coronavirus OC43',
-    # COVID spike in kraken
-    'Betacoronavirus 1',
-    'Rabbit coronavirus HKU14',
-    # COVID spike in CZID
-    'Betacoronavirus',
-    # Adenovirus
-    'Human adenovirus 1',
-    'Human mastadenovirus A',
-    'Human mastadenovirus C',
-    'Human mastadenovirus F',
-    'Human mastadenovirus G',
-    'Simian mastadenovirus B',
-    'Simian mastadenovirus C',
-    'Simian mastadenovirus F',
-    'adeno-associated virus 2',
-    'Mastadenovirus',
-    # Flu
-    'Alphainfluenzavirus influenzae',
-]
-
-
-def load_data(
-    glb: str,
-    remove_spike: bool | str,
-    exp_files=36,
-    normalize=True,
-    phylum_mode=False,
-) -> pd.DataFrame:
-    files = glob(glb)
-    files = [f for f in files if 'Undetermined' not in f]
-    if exp_files:
-        assert len(files) == exp_files, f'expected {exp_files}, got {len(files)}'
-    print(len(files), 'files')
-    jsons = (json.load(open(f)) for f in files)
-
-    df = (
-        pd.DataFrame(jsons)
-        .assign(name=[fix_name(f) for f in files])
-        .set_index('name')
-        .replace(np.nan, 0)
-        .sort_index()
-    )
-    df = sort_columns(df)
-
-    if phylum_mode:
-        # Split columns into species and phylum.
-        # Using species for spike removal, then assigning phylum again.
-        spc, phy = [], []
-        for c in df.columns.tolist():
-            s = c.split(',')
-            assert len(s) == 2, f'{c!r}: {len(s)} parts'
-            spc.append(s[0])
-            phy.append(s[1])
-        df.columns = spc
-
-    if remove_spike:
-        keep = [True] * df.shape[1]
-        print('Removing spike:')
-        for x in SPIKE_TAXA:
-            if x in df:
-                if type(remove_spike) is str:
-                    df[remove_spike] += df[x]
-                keep[df.columns.get_loc(x)] = False
-                # del df[x]
-                print('-', x)
-        df = df.loc[:, keep]
-
-    if phylum_mode:
-        # Sum by phylum.
-        phy = [x for x, k in zip(phy, keep) if k]
-        df.columns = phy
-        df = df.groupby(df.columns, axis=1).sum()
-
-    if normalize:  # Normalize to 1
-        df = df.div(df.sum(axis=1), axis=0)
-
-    return df
 
 
 def load_human_host():
@@ -198,12 +104,6 @@ def nsubplots(n) -> np.ndarray:
 def sorted_by_time(a) -> list:
     no_prefix = re.compile('.*?_(.*)')
     return sorted(a, key=lambda x: no_prefix.findall(x)[0])
-
-
-def sort_columns(df: pd.DataFrame) -> pd.DataFrame:
-    column_sums = df.sum(axis=0)
-    column_sums = column_sums.sort_values(ascending=False)
-    return df.reindex(columns=column_sums.index)
 
 
 def repl_strings(s: str, d: dict) -> str:
