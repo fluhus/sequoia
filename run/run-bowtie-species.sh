ed5265c501aef1d9a77b3b16fc34d3d4720d7b85
Amit Lavon <amitlavon1.spam@gmail.com>
2025-04-24 15:41:47 -0700

    Add bowtie run file

---
+++ run/run-bowtie-species.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+#SBATCH -c 4
+#SBATCH --mem 1g
+#SBATCH -p free
+
+#aSBATCH -A katrine_lab
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "no input dir"
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "no ref dir"
+  exit 1
+fi
+if [ -z "$3" ]; then
+  echo "no output dir"
+  exit 1
+fi
+
+indir="$1"
+refdir="$2"
+outdir="$3"
+
+i=$SLURM_ARRAY_TASK_ID
+nrefs=$(find "$refdir" -name \*.fasta | wc -l)
+isample=$(((i-1) / nrefs + 1))
+iref=$(((i-1) % nrefs + 1))
+
+r=$(find "$refdir" -name \*.fasta | sort | sed 's/.fasta//' | line $iref)
+rb=$(basename "$r")
+s=$(ls -1S "$indir" | grep .1.fastq.zst | sed 's/.1.fastq.zst//' | line $isample)
+
+echo "Sample:    ($isample) $s"
+echo "Reference: ($iref) $rb"
+
+bowtie2 -p 4 --no-head --no-unal -t --very-fast \
+  -x $r \
+  -1 $indir/$s.1.fastq.zst \
+  -2 $indir/$s.2.fastq.zst \
+  -S >(zstd > $outdir/$rb.$isample.sam.zst)
+
+sleep 1
+zstd --test $outdir/$rb.$isample.sam.zst
+
+echo Done
